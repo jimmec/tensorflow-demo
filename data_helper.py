9ac3ff21387e414f47d8b4a61286f3b64c0f33c3
Jimmy Cao <jimmec@gmail.com>
2015-12-16 03:02:07 -0800

    added CNN graph implementation and train script from tutorial

--- data_helper.py
+++ data_helper.py
@@ -133,4 +133,18 @@ def load_input_data(example_path, vocab_path):
         vocab_map = pickle.load(v)
     return build_input_data(sentences, labels, vocab_map['vocabulary'], vocab_map['inv_vocabulary'])
 
-preprocess_raw_inputs_and_save()
+def batch_iter(data, batch_size, num_epochs):
+    """
+    Generates a batch iterator for a dataset.
+    """
+    data = np.array(data)
+    data_size = len(data)
+    num_batches_per_epoch = int(len(data)/batch_size) + 1
+    for epoch in range(num_epochs):
+        # Shuffle the data at each epoch
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_data = data[shuffle_indices]
+        for batch_num in range(num_batches_per_epoch):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, data_size)
+            yield shuffled_data[start_index:end_index]
