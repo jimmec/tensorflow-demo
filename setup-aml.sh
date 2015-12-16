eaaafce8fd18817a5fe600de506e848d906d86bc
EC2 Default User <ec2-user@ip-172-31-15-49.us-west-2.compute.internal>
2015-12-16 03:58:16 +0000

    added AML setup and pip requirements

---
+++ setup-aml.sh
@@ -0,0 +1,7 @@
+sudo yum groupinstall 'Development Tools'\n
+sudo yum install git
+sudo yum install atlas-devel atlas
+sudo yum install ncurses-devel ncurses
+sudo yum install openssl-devel
+sudo yum install -y lapack lapack-devel blas blas-devel blas-static lapack-static libgfortran
+sudo yum install libffi-devel
